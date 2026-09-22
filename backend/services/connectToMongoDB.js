836ea56eaa56574024e17190be7acb804095f8d1
Anupam Yadav <anupamy571@gmail.com>
2026-09-22 22:08:02 +0530

    mongodb URI updated

--- backend/services/connectToMongoDB.js
+++ backend/services/connectToMongoDB.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-  const db_password = process.env.MONGODB_PASSWORD;
-
   try {
-    (await mongoose.connect(
-      `mongodb+srv://forlaptop21_db_user:${db_password}@cluster0.u4qdjze.mongodb.net/?appName=Cluster0`,
-    ),
+    (await mongoose.connect(process.env.MONGODB_URI),
       {
         autoSelectFamily: false,
       });
